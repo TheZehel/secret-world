308557c5d7a2eb79ee07021bce0e72c86384763d
TheZehel <alls.yuri@gmail.com>
2023-05-05 22:56:25 -0300

    estrutura de componentes e picks e letters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,88 @@
+// CSS
 import './App.css';
 
+// React
+import { useCallback, useEffect, useState } from 'react';
+
+// Data
+import { wordsList } from './data/words';
+
+//Components
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+    { id: 1, name: 'start' },
+    { id: 2, name: 'game' },
+    { id: 3, name: 'end' },
+];
+
 function App() {
-  return (
-    <div className="App">
-      <h2>Secret World</h2>
-    </div>
-  );
+    const [gameStage, setGameStage] = useState(stages[0].name);
+    const [words] = useState(wordsList);
+
+    const [pickedWord, setPickedWord] = useState('');
+    const [pickedCategory, setPickedCategory] = useState('');
+    const [letters, setLetters] = useState([]);
+
+    const pickWordAndPickCategory = () => {
+        // pick a random category
+        const categories = Object.keys(words);
+        const category =
+            categories[
+                Math.floor(Math.random() * Object.keys(categories).length)
+            ];
+
+        console.log(category);
+
+        //pick a random word
+        const word =
+            words[category][Math.floor(Math.random() * words[category].length)];
+
+        console.log(word);
+
+        return { word, category };
+    };
+
+    // starts the secret world
+    const startGame = () => {
+        //pick word and pick category
+        const { word, category } = pickWordAndPickCategory();
+
+        //create an array of letters
+        let wordLetters = word.split('');
+
+        wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+        console.log(word, category);
+        console.log(wordLetters);
+
+        //fill states
+        setPickedWord(word);
+        setPickedCategory(category);
+        setLetters(letters);
+
+        setGameStage(stages[1].name);
+    };
+
+    // process the letter input
+    const verifyLetter = () => {
+        setGameStage(stages[2].name);
+    };
+
+    //Restarts The Game
+    const retry = () => {
+        setGameStage(stages[0].name);
+    };
+
+    return (
+        <div className="App">
+            {gameStage === 'start' && <StartScreen startGame={startGame} />}
+            {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+            {gameStage === 'end' && <GameOver retry={retry} />}
+        </div>
+    );
 }
 
 export default App;
